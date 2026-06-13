f53359a710c9765da430b49355f6e4f542da3f7e
Admin <admin@buzzwiredaily.com>
2026-06-14 00:38:08 +0600

    Update Author URI to corrected base domain URL

--- signalkit.php
+++ signalkit.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) { exit; }
  * Requires at least: 5.3
  * Requires PHP: 7.2
  * Author: N4Nion
- * Author URI: https://signalkit.wikiofautomation.com/home/
+ * Author URI: https://signalkit.wikiofautomation.com/
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: signalkit
